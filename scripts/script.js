aa12aafa7a8544e3dc6c1ebd627d48b8b9eafe38
lilia.dmy <72511746+aileen-tech@users.noreply.github.com>
2026-04-08 20:53:54 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+function afficherResultat(score, nbMotsProposes) {
+    let spanScore = document.querySelector(".zoneScore span")
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    spanScore.innerText = affichageScore
+}
+
+
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerText = proposition
+}
+
+
+
+function lancerJeu() {
+    let score = 0
+    let i = 0
+
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
+    afficherProposition(listeMots[i])
+    btnValiderMot.addEventListener("click", () => {
+        console.log(inputEcriture.value)
+        if (inputEcriture.value === listeMots[i]) {
+            score++
+        }
+        i++
+        afficherResultat(score, i)
+        inputEcriture.value = ''
+        if (listeMots[i] === undefined) {
+            afficherProposition("Le jeu est fini")
+            btnValiderMot.disabled = true
+        } else {
+            afficherProposition(listeMots[i])
+        }
+        
+    })
+
+    afficherResultat(score, i)
+}
